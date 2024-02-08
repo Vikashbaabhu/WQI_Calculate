ba21398120724060e49cfc35977ee6cee63feddd
Vikash Baabhu <75823844+Vikashbaabhu@users.noreply.github.com>
2024-02-08 20:06:14 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -26,7 +26,7 @@ def predict_note_authentication(year,temp,do,ph,con,bod,n,fc,tc):
 def main():
     html_temp = """
     <div style="background-color:skyblue;padding:10px;border-radius:5px">
-    <h2 style="color:white;text-align:center;font-family:Callibri;"><b>Water Quality Index Calculation</b></h2>
+    <h2 style="color:darkblue;text-align:center;font-family:Callibri;"><b>Water Quality Index Calculation</b></h2>
     </div>
     <br></br>
     """
@@ -34,41 +34,41 @@ def main():
     st.markdown(html_temp,unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
     #temp min - 18s
-    temp = col1.number_input("Temperature")
+    temp = col1.number_input("**Temperature**")
     # do min - 5.5
-    do = col2.number_input("Dissolved Oxygen")
+    do = col2.number_input("**Dissolved Oxygen**")
     #ph min - 7.2
-    ph = col3.number_input("pH")
+    ph = col3.number_input("**pH**")
     #con min - 275
-    con = col1.number_input("Conductivity ")
+    con = col1.number_input("**Conductivity**")
     #bod min - 3
-    bod = col2.number_input("Bio-chemical Oxygen Demand")
+    bod = col2.number_input("**Bio-chemical Oxygen Demand**")
     #n min - 0.5
-    n = col3.number_input("Nitrate")
+    n = col3.number_input("**Nitrate**")
     #fc min- 2830
-    fc = col1.number_input("Fecal Coliform ")
+    fc = col1.number_input("**Fecal Coliform**")
     #tc min - 8000
-    tc = col3.number_input("Total Coliform ")
+    tc = col3.number_input("**Total Coliform**")
     result=""
     col2.text("")
     col2.text("")
-    if col2.button("Calculate"):
+    if col2.button("**Calculate**"):
         result=predict_note_authentication(2024,temp,do,ph,con,bod,n,fc,tc)
-        st.success('The Water Quality Index of the river is {:.2f}'.format(result[0]))
+        st.success('**The Water Quality Index of the river is {:.2f}**'.format(result[0]))
         if result <= 25 :
-          st.success('The quality of the water is excellent')  
+          st.success('**The quality of the water is excellent**')  
           image = Image.open('Excellent.jpg')
           st.image(image, caption='Excellent')
         elif result >= 25 and result <= 50:
-          st.success('The quality of the water is good')  
+          st.success('**The quality of the water is good**')  
           image = Image.open('Good.jpg')
           st.image(image, caption='Good')
         elif result >= 50 and result <= 75:
-          st.success('The quality of the water is poor')
+          st.success('**The quality of the water is poor**')
           image = Image.open('Poor.jpg')
           st.image(image, caption='Poor')
         else:
-          st.success('The quality of the water is very poor')
+          st.success('**The quality of the water is very poor**')
           image = Image.open('Very Poor.jpg')
           st.image(image, caption='Very Poor')
 
